e857ca88adf2cf0fc7aba12aea30da01c412b59d
Dinesh <dineshchhangani9928@gmail.com>
2024-07-10 13:56:20 +0530

    ecommerce app with bloc

--- lib/presentation/screens/on_board/sign_up_bloc/sign_bloc.dart
+++ lib/presentation/screens/on_board/sign_up_bloc/sign_bloc.dart
@@ -10,12 +10,12 @@ class SignupBloc extends Bloc<SignupEvents,SignupState>{
   SignupBloc({required this.apiHelper}):super(SInitState()){
     on<SignEvent>((event, emit) async {
       try{
-         var data=await apiHelper.postApi(url: GetApiUrl.REGISTRATION_URL,mapData: {'name': event.name, 'mobile_number': event.mobile_number, 'email': event.email, 'password': event.password},)as JsonResponse;
-         if(data.status!){
+         var data=await apiHelper.postApi(url: GetApiUrl.REGISTRATION_URL,mapData: {'name': event.name, 'mobile_number': event.mobile_number, 'email': event.email, 'password': event.password},);
+         var loginData=JsonResponse(status: data['status'], message: data['message'], token: data['tokan']);
+         if(loginData.status!){
            emit(SLoadedState());
          }else{
-
-           emit(SErrorState(msg:'${data.message}'));
+           emit(SErrorState(msg:'${loginData.message}'));
          }
       }catch(e){
         emit(SErrorState(msg: (e.toString())));
